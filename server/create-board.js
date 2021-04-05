55e9d624a27e26c5eeded23af77ca0195201dbdf
lacau <lacau@zpay.com.br>
2021-04-05 11:47:58 -0300

    Store board state at server side

---
+++ server/create-board.js
@@ -0,0 +1,21 @@
+const createBoard = (size) => {
+    let board;
+
+    const clear = () => {
+        board = Array(size).fill().map(() => Array(size).fill(null));
+    };
+
+    const getBoard = () => board;
+
+    const makeTurn = (x, y, color) => {
+        board[y][x] = color;
+    };
+
+    clear();
+
+    return {
+        clear, getBoard, makeTurn
+    };
+}
+
+module.exports = createBoard;
